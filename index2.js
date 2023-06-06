ae1fe9c1909858d0271e4a49271d53de55ab238b
Sadki <98910363+Sadki10@users.noreply.github.com>
2023-06-06 13:06:44 +0200

    Delete index2.js

--- index2.js
+++
@@ -1,9 +0,0 @@
-const password = document.getElementById('password').value;
-const confirmPasswd = document.getElementById('confirmPasswd').value;
-const form = document.getElementById('form');
-
-form.addEventListener('submit', e=>{
-    if (password === confirmPasswd) {
-        console.log('hola');
-    }
-})
